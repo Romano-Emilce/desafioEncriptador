daeee097b2609d251a2c919752de96f2722fb895
Emilce Romano <romano.emilce.belen@gmail.com>
2024-03-14 22:33:58 -0300

    subiendo desafio ONE

---
+++ app.js
@@ -0,0 +1,86 @@
+const textArea = document.getElementById("textArea");
+
+const btnEncriptador = document.getElementById("btnEncriptador");
+
+const btnDesencriptador = document.getElementById("btnDesencriptador");
+
+const contenidoFinal = document.getElementById("contenidoFinal");
+
+const btnCopiar = document.getElementById("btnCopiar");
+
+const textoPresentacion = document.getElementById ("textoPresentacion");
+
+const imagen = document.getElementById ("imagen");
+
+let remplazar = [
+    ["e", "enter"],
+    ["i", "imes"],
+    ["a", "ai"],
+    ["o", "ober"],
+    ["u", "ufat"],
+]
+
+
+
+btnEncriptador.addEventListener("click", () => {
+    const texto = textArea.value.toLowerCase ()
+    function encriptar(nuevoTexto){
+        for (let i = 0; i < remplazar.length; i++){
+            if (nuevoTexto.includes(remplazar [i] [0])){
+                nuevoTexto = nuevoTexto.replaceAll(remplazar[i][0], remplazar[i][1]);
+            };
+        };
+        return nuevoTexto
+    }
+
+    const textoEncriptado = encriptar (texto)
+
+    contenidoFinal.innerHTML = textoEncriptado
+
+    imagen.style.display = "none";
+
+    textoPresentacion.style.display = "none";
+
+    btnCopiar.style.display = "block";
+    //seguir modificando los estilos
+
+    console.log (encriptar(texto))
+});
+
+btnDesencriptador.addEventListener("click", () => {
+    const texto = textArea.value.toLowerCase(); 
+    function desencriptar(nuevoTexto) {
+        for (let i = 0; i < remplazar.length; i++) {
+            if (nuevoTexto.includes(remplazar[i][1])) {
+                nuevoTexto = nuevoTexto.replaceAll(remplazar[i][1], remplazar[i][0]);
+            };
+        };
+        return nuevoTexto;
+    }
+     
+    const textoDesencriptado = desencriptar(texto);
+
+    contenidoFinal.innerHTML = textoDesencriptado;
+});
+
+btnCopiar.addEventListener("click", () => {
+    let textoEncriptado = contenidoFinal.textContent;
+
+    const elementoTemporal = document.createElement("textarea");
+    elementoTemporal.value = textoEncriptado;
+    elementoTemporal.setAttribute("readonly", "");
+    elementoTemporal.style.position = "absolute";
+    elementoTemporal.style.left = "-9999px";
+    elementoTemporal.style.overflow = "hidden";
+    document.body.appendChild(elementoTemporal);
+
+    elementoTemporal.select();
+    document.execCommand("copy");
+
+    document.body.removeChild(elementoTemporal);
+
+    alert("¡Texto copiado correctamente!");
+});
+
+
+contenidoFinal.addEventListener
